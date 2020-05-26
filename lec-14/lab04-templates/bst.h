c384a3fea453f7dde6d33042c3a7a0f63bb09725
Diba Mirza <dimirza@cs.ucsb.edu>
2020-05-26 10:45:59 -0700

    lec 14

--- lec-14/lab04-templates/bst.h
+++ lec-14/lab04-templates/bst.h
@@ -1,5 +1,8 @@
-// BST.h
+// bst.h
 // Modified version of lab04 starter code for demo purposes
+// changed name of class from IntBST<T> -> BST<T> (Generic, templates)
+// Generic? keys (int), any type of key
+// shorter list of member functions
 
 #ifndef BST_H
 #define BST_H
@@ -8,34 +11,47 @@
 
 using namespace std;
 
-class BST {
+/*
+template <class W>
+struct Node {
+        W info;
+        Node *left, *right, * parent;
+        // useful constructor:
+        Node(W v) : info(v), left(0), right(0), parent(0) { }
+};
+Node<int> n1;
+*/
 
+template <class T> // BST<T><int> BST<T>1; BST<T><string> BST<T>2: T->type auto x = 9;
+class BST {
  public:
-    // ctor, dtor, insert and one print method already done in BST.cpp:
+    // ctor, dtor, insert and one print method already done in BST<T>.cpp:
     BST();                   // constructor
     ~BST();                  // destructor
-    bool insert(int value);     // insert value; return false if duplicate
-    bool contains(int value) const;  // true if value is in tree
+    bool insert(T value);     // insert value; return false if duplicate
+    bool contains(T value) const;  // true if value is in tree
     void printPreOrder() const; // prints tree data pre-order to cout
 
  private:
 
     struct Node {
-	int info;
-	Node *left, *right, * parent;
-	// useful constructor:
-    Node(int v=0) : info(v), left(0), right(0), parent(0) { }
+        T info;
+        Node *left, *right, * parent;
+        // useful constructor:
+        Node(T v) : info(v), left(0), right(0), parent(0) { }
     };
 
     // just one instance variable (pointer to root node):
-    Node *root;
+    Node* root;
 
     // recursive utility functions for use by public methods above
-    Node* getNodeFor(int value, Node* n) const; // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
+    Node* getNodeFor(T value, Node* n) const; // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
     void clear(Node *n); // for destructor
-    bool insert(int value, Node *n); // note overloading names for simplicity
+    bool insert(T value, Node *n); // note overloading names for simplicity
     void printPreOrder(Node *n) const;
     
 };
+
+#include "bst.template"
 
 #endif
--- lec-14/lab04-templates/testbst.cpp
+++ lec-14/lab04-templates/testbst.cpp
@@ -6,26 +6,45 @@
 #include <vector>
 using namespace std;
 
-void insertIntoBST(BST& b, vector<int>& v)
+// Make the function generic using templates
+template <class item>
+void insertIntoBST(BST<item>& b, vector<item>& v)
 {
-  // insert data to bst1
-    for(auto item: v){
-        b.insert(item);
+  // insert data to BST<T>1
+    for(auto i: v){
+        b.insert(i);
     }
 
 }
 
+struct mytype{
+  int a;
+  string b;
+};
 int main() {
     vector<int> v1{64, 128, 8, 512, 16, 4};
-    BST bst1;  
+    BST<int> bst1; 
+   // BST<mytype> b3;  // overload the ==, < operators for myttype
+
     insertIntoBST(bst1, v1);
 
-    cout << "  pre-order: ";
+    cout << " pre-order: ";
     bst1.printPreOrder();
     cout << endl;
 
-  
+    vector<string> v2{"Apple", "Orange", "Banana"};
+    BST<string> bst2;
+    insertIntoBST(bst2, v2);
+    cout << "  pre-order: ";
+    bst2.printPreOrder();
+    cout << endl;
     return 0;
 }
+/*
+Lab04 extra credit
+A. Tried and completed it
+B. Tried
+C. Haven't started
+*/
 
 
